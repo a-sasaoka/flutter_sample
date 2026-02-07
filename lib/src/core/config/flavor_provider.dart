3ba8d080c8f4fb386222c6bbce21d4f25761967f
a-sasaoka <chikezo@gmail.com>
2026-02-07 16:08:48 +0900

    Firebaseの設定切り替え方法を変更＆Flavor用のプロバイダーを追加

---
+++ lib/src/core/config/flavor_provider.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'flavor_provider.g.dart';
+
+/// Flavorを扱うProvider
+@Riverpod(keepAlive: true)
+Flavor flavor(Ref ref) {
+  throw UnimplementedError();
+}
+
+/// Flavorの定義
+enum Flavor {
+  /// ローカル環境
+  local,
+
+  /// 開発環境
+  dev,
+
+  /// ステージング環境
+  stg,
+
+  /// 本番環境
+  prod,
+}
